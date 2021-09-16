31469d9c7eb3bcd3bbd67d42fb363107f4ab5d68
Tatiana Chavez <tchavez2@HHS-DO-E053731.local>
2021-09-16 09:32:22 -0700

    Added new requirements to requirements file and recompiled in order to use plotly lib. Added code to make scatter plots, scatterplot matrix, violin plot, box plot, and marginal distribution plots

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+import plotly.express as px 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # importing data into dataframe with correct labels
 header_list = [
@@ -41,7 +44,7 @@ def print_stats(iris_stats):
         print(header_list[x] + ": " + str(iris_stats[x]))
     return ()
 
-
+#find and print results
 print("Stats Summary for Iris Data")
 print("-------------------------------")
 print("Overall")
@@ -55,3 +58,81 @@ get_stats(iris_versicolor)
 print("-------------------------------")
 print("Iris Virginica")
 get_stats(iris_virginica)
+
+
+#plotly sections
+#creating a scatter plot using tutorial from: https://plotly.com/python/line-and-scatter/
+fig = px.scatter(iris_df, x="sepal width in cm", y="sepal length in cm", color="class",
+                 size='petal length in cm', hover_data=['petal width in cm'], title="Iris Scatter Plot")
+fig.update_layout(
+    font_family="Helvetica",
+    font_size=18
+)
+fig.show()
+
+#scatterplot matrix using tutorial from: https://plotly.com/python/splom/
+fig = px.scatter_matrix(iris_df,
+    dimensions=["sepal length in cm", "sepal width in cm", "petal length in cm", "petal width in cm"],
+    color="class", symbol="class",
+    title="Scatter matrix of iris data set")
+fig.update_traces(diagonal_visible=False)
+fig.update_layout(
+    font_family="Helvetica",
+    font_size=18
+)
+fig.show()
+
+#create a violin plot using tutorial from: 
+fig = go.Figure()
+
+fig.add_trace(go.Violin(x=iris_df['class'],
+                        y=iris_df['sepal length in cm'],
+                        legendgroup='sepal length in cm', scalegroup='sepal length in cm', name='sepal length in cm',
+                        line_color='#404040')
+             )
+
+fig.add_trace(go.Violin(x=iris_df['class'],
+                        y=iris_df['sepal width in cm'],
+                        legendgroup='sepal width in cm', scalegroup='sepal width in cm', name='sepal width in cm',
+                        line_color='#2a6a6c')
+             )
+
+fig.add_trace(go.Violin(x=iris_df['class'],
+                        y=iris_df['petal length in cm'],
+                        legendgroup='petal length in cm', scalegroup='petal length in cm', name='petal length in cm',
+                        line_color='#f29724')
+             )
+
+fig.add_trace(go.Violin(x=iris_df['class'],
+                        y=iris_df['petal width in cm'],
+                        legendgroup='petal width in cm', scalegroup='petal width in cm', name='petal width in cm',
+                        line_color='#b90d49')
+             )
+
+fig.update_traces(box_visible=True, meanline_visible=True)
+fig.update_layout(violinmode='group',title='Isis Violin Plot')
+fig.show()
+
+#boxplot from:https://plotly.com/python/box-plots/#grouped-horizontal-box-plot
+fig = px.box(iris_df, y=['sepal length in cm', 'sepal width in cm', 'petal length in cm', 'petal width in cm'], color="class")
+fig.update_traces(quartilemethod="exclusive") # or "inclusive", or "linear" by default
+fig.update_layout(title='Isis Boxplot',font_family="Helvetica",font_size=18)
+fig.show()
+
+#Marginal Distribution Plots ref: https://plotly.com/python/marginal-plots/
+fig = px.scatter(iris_df, x="sepal length in cm", y="sepal width in cm", color="class", marginal_x='box', marginal_y="violin", title ="Sepal Length vs Sepal Width")
+fig.update_layout(
+    font_family="Helvetica",
+    font_size=18
+)
+fig.show()
+
+fig = px.scatter(iris_df, x="petal length in cm", y="petal width in cm", color="class", 
+                 marginal_x="box", marginal_y="violin", title="Petal Length vs Petal Width")
+fig.update_layout(
+    font_family="Helvetica",
+    font_size=18
+)
+fig.show()
+
+
